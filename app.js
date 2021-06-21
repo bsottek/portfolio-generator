538372e27b3bfe716a0f790229a9b4e0e3484108
bsottek <bsottek@gmail.com>
2021-06-21 10:51:58 -0500

    Generate file based on user inputs. Modularized functions.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-printProfileData(profileDataArgs);
+    console.log('Porfolio complete! Check index.html to see the output.');
+});
