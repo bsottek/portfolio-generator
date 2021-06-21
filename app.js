b1cd988a853311c5cc2ec7c7295cfe4e64fd5d31
bsottek <bsottek@gmail.com>
2021-06-21 13:57:39 -0500

    Answer validation for required questions

--- app.js
+++ app.js
@@ -18,7 +18,15 @@ const promptUser = () => {
             {
                 type: 'input',
                 name: 'github',
-                message: 'Enter your GitHub username.'
+                message: 'Enter your GitHub username.',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your name');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
@@ -43,12 +51,28 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -59,7 +83,15 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
