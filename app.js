693df16b8d6cb561b12eb067782ffe717402fbcc
bsottek <bsottek@gmail.com>
2021-06-21 11:31:51 -0500

    Install inquirer and adapt app.js to request/store input

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-    console.log('Porfolio complete! Check index.html to see the output.');
-});
+//     console.log('Porfolio complete! Check index.html to see the output.');
+// });
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
